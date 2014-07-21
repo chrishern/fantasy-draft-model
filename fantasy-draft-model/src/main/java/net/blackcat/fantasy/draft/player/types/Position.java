f21c5bf0f4ca03b7c7e48a8989e588e966e8d99c
chrishern <chrishern@hotmail.com>
2014-07-21 21:05:38 +0100

    Added position enum.

---
+++ fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/player/types/Position.java
@@ -0,0 +1,15 @@
+package net.blackcat.fantasy.draft.player.types;
+
+/**
+ * Type representing all available positions for a player within the fantasy draft.
+ * 
+ * @author Chris
+ *
+ */
+public enum Position {
+
+	GOALKEEPER,
+	DEFENDER,
+	MIDFIEDER,
+	STRIKER;
+}
