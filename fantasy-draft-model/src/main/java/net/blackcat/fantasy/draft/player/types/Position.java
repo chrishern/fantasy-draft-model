7858ed996e89b8769da1d33f34f87fc4b71663ec
chrishern <chrishern@hotmail.com>
2014-07-24 23:05:51 +0100

    Added mapping of Position enum to the FPL string position value.

--- fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/player/types/Position.java
+++ fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/player/types/Position.java
@@ -8,8 +8,37 @@ package net.blackcat.fantasy.draft.player.types;
  */
 public enum Position {
 
-	GOALKEEPER,
-	DEFENDER,
-	MIDFIEDER,
-	STRIKER;
+	GOALKEEPER("Goalkeeper"),
+	DEFENDER("Defender"),
+	MIDFIEDER("Midfielder"),
+	STRIKER("Forward");
+	
+	private String fantasyPremierLeagueValue;
+	
+	private Position(final String fantasyPremiumLeagueValue) {
+		this.fantasyPremierLeagueValue = fantasyPremiumLeagueValue;
+	}
+	
+	/**
+	 * @return the fantasyPremierLeagueValue
+	 */
+	public String getFantasyPremierLeagueValue() {
+		return fantasyPremierLeagueValue;
+	}
+	
+	/**
+	 * Convert the string FPL position value to the enum value.
+	 * 
+	 * @param fantasyPremierLeaguePosition String value of the position from the FPL.
+	 * @return Equivalent Position enum.
+	 */
+	public static Position fromFantasyPremierLeaguePosition(final String fantasyPremierLeaguePosition) {
+		for (final Position position : Position.values()) {
+			if (position.getFantasyPremierLeagueValue().equals(fantasyPremierLeaguePosition)) {
+				return position;
+			}
+		}
+		
+		return null;
+	}
 }
