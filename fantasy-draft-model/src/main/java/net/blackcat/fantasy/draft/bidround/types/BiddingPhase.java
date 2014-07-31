b38c6323747a18c337da05f46add9ab4e6c60560
chrishern <chrishern@hotmail.com>
2014-07-31 20:09:39 +0100

    Added bidding phase enum.

---
+++ fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/bidround/types/BiddingPhase.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.blackcat.fantasy.draft.bidround.types;
+
+/**
+ * Enum representing the different types of bidding phases within the fantasy draft.
+ * 
+ * @author Chris
+ *
+ */
+public enum BiddingPhase {
+
+	AUCTION,
+	TRANSFER_WINDOW;
+}
