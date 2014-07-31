75fcaeaeb61760bb6feace132ffa94e47abaab12
chrishern <chrishern@hotmail.com>
2014-07-31 20:22:46 +0100

    Added type for the status of a bidding phase.

---
+++ fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/bidround/types/BiddingPhaseStatus.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.blackcat.fantasy.draft.bidround.types;
+
+/**
+ * Enum representing the status (i.e. open/closed/etc) of a bidding phase.
+ * 
+ * @author Chris
+ *
+ */
+public enum BiddingPhaseStatus {
+
+	OPEN,
+	CLOSED;
+}
