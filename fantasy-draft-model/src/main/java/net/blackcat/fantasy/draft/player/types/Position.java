5ea63ca446f534416f9836a708d52342a4b366f4
chrishern <chrishern@hotmail.com>
2014-08-02 22:29:35 +0100

    Added mapping from REST API value.

--- fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/player/types/Position.java
+++ fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/player/types/Position.java
@@ -8,15 +8,17 @@ package net.blackcat.fantasy.draft.player.types;
  */
 public enum Position {
 
-	GOALKEEPER("Goalkeeper"),
-	DEFENDER("Defender"),
-	MIDFIEDER("Midfielder"),
-	STRIKER("Forward");
+	GOALKEEPER("Goalkeeper", "goalkeepers"),
+	DEFENDER("Defender", "defenders"),
+	MIDFIEDER("Midfielder", "midfielders"),
+	STRIKER("Forward", "strikers");
 	
 	private String fantasyPremierLeagueValue;
+	private String restApiValue;
 	
-	private Position(final String fantasyPremiumLeagueValue) {
+	private Position(final String fantasyPremiumLeagueValue, final String restApiValue) {
 		this.fantasyPremierLeagueValue = fantasyPremiumLeagueValue;
+		this.restApiValue = restApiValue;
 	}
 	
 	/**
@@ -27,6 +29,13 @@ public enum Position {
 	}
 	
 	/**
+	 * @return the restApiValue
+	 */
+	public String getRestApiValue() {
+		return restApiValue;
+	}
+
+	/**
 	 * Convert the string FPL position value to the enum value.
 	 * 
 	 * @param fantasyPremierLeaguePosition String value of the position from the FPL.
@@ -35,6 +44,22 @@ public enum Position {
 	public static Position fromFantasyPremierLeaguePosition(final String fantasyPremierLeaguePosition) {
 		for (final Position position : Position.values()) {
 			if (position.getFantasyPremierLeagueValue().equals(fantasyPremierLeaguePosition)) {
+				return position;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Convert the string REST API position value to the enum value.
+	 * 
+	 * @param restApiValue String value of the position from the REST API parameter.
+	 * @return Equivalent Position enum.
+	 */
+	public static Position fromRestApiValue(final String restApiValue) {
+		for (final Position position : Position.values()) {
+			if (position.getRestApiValue().equals(restApiValue)) {
 				return position;
 			}
 		}
