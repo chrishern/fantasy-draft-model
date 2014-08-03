39a24dd935d765d3f5ed4a902de1d9d0025b6cae
chrishern <chrishern@hotmail.com>
2014-08-03 22:29:04 +0100

    Added selected players to the team class.

--- fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/team/Team.java
+++ fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/team/Team.java
@@ -4,6 +4,9 @@
 package net.blackcat.fantasy.draft.team;
 
 import java.io.Serializable;
+import java.util.List;
+
+import net.blackcat.fantasy.draft.player.Player;
 
 /**
  * Model object representing a fantasy draft team.
@@ -16,6 +19,7 @@ public class Team implements Serializable {
 	private static final long serialVersionUID = -6114339036617218927L;
 
 	private String teamName;
+	private List<Player> selectedPlayers;
 
 	public Team(final String teamName) {
 		this.teamName = teamName;
@@ -34,4 +38,19 @@ public class Team implements Serializable {
 	public void setTeamName(final String teamName) {
 		this.teamName = teamName;
 	}
+
+	/**
+	 * @return the selectedPlayers
+	 */
+	public List<Player> getSelectedPlayers() {
+		return selectedPlayers;
+	}
+	
+	/**
+	 * @param selectedPlayers the selectedPlayers to set
+	 */
+	public void setSelectedPlayers(final List<Player> selectedPlayers) {
+		this.selectedPlayers = selectedPlayers;
+	}
 }
+
