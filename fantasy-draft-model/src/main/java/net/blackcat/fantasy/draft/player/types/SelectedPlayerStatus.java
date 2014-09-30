af92d0379843dcf1e764c1e4a944fd0de936ee95
chrishern <chrishern@hotmail.com>
2014-09-30 20:18:53 +0100

    Added new enum storing the status of the selected players.

---
+++ fantasy-draft-model/src/main/java/net/blackcat/fantasy/draft/player/types/SelectedPlayerStatus.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package net.blackcat.fantasy.draft.player.types;
+
+/**
+ * Type storing the different status' that a selected player can have.
+ * 
+ * @author Chris
+ *
+ */
+public enum SelectedPlayerStatus {
+
+	STILL_SELECTED,
+	PENDING_SALE_TO_POT,
+	PENDING_TRANSFER_OUT,
+	SOLD_TO_POT,
+	TRANSFERRED_OUT;
+}
